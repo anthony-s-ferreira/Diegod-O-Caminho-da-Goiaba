79fdd46af5603c42ae73b8bdb624d31689f86999
Otávio Cipriano <mirllamsa@gmail.com>
2022-08-13 19:36:48 -0300

    Eventos e personagens

---
+++ Prolog/Bodys.pl
@@ -0,0 +1,18 @@
+none():- write("('_')"), nl, write(" ( )"), nl, 
+write(" L |"), nl, nl, write("Digite J para jogar o dado").
+
+foundBox():- write("(!_!)  ______"), nl, write(" ( )-  |    |"), nl, 
+write(" L |   |____|"), nl, nl, write("Digite J para jogar o dado e A para abrir a caixa").
+
+cinquentinha():- write(" (#_#)  "), nl, write("  ( )-!"), nl, 
+write(" |----| "),nl, write(" @    @"), nl, nl, write("Digite J para jogar o dado").
+
+calopisitaGigante():- write(" (^_^)   (^)>"), nl, write(" \\( )/   (/)"), nl, 
+write("  L |    ''"), nl, nl, write("Digite J para jogar o dado").
+
+goibaDourada():- write("(^_^)    \\ | /"), nl, write(" \\( )/   - ( ) -"), nl, 
+write("  L |     / | \\"), nl, nl, write("Digite J para jogar o dado").
+
+baculejoMoto():- write("         ___"), nl, write("       _| * |_"), nl, 
+write(" (-_-)  (-_-)       -!"),  nl, write("  ( )   <( )>   |----| "), 
+nl, write("  L |    L L    @    @"), nl, nl, write("Digite J para jogar o dado").
---
+++ Prolog/Events.pl
@@ -0,0 +1,19 @@
+
+noEvent() :- write("Nada aconteceu!"), nl, nl, 
+write("Jogar dado (ganha pontos)"), nl, nl.
+
+box():- write("Diegod achou uma caixa! Deseja abrir?"), nl, nl, 
+write("Abrir caixa (-10 pontos + Power up)  Jogar dado (ganha pontos)"), 
+nl, nl.
+
+moto():- write("Diegod ganhou uma 50tinha, pontuação x2 por 5 rodadas."),nl, 
+write("Cuidado com os policiais!"), nl, nl, write("Jogar dado (ganha pontos)"), nl, nl.
+
+calopisita():- write("Diegod achou a calopsita gigante, pontuação x3 por 5 rodadas."),
+nl, nl, write("Jogar dado (ganha pontos)"), nl, nl.
+
+goiaba():- write("Diegod achou a goiaba dourada, não perderá pontos ou morrerá!"),nl, 
+write("A goiaba dourada só será usada uma vez."),nl, nl, write("Jogar dado (ganha pontos)"), nl, nl.
+
+baculejoMotoquinha():- write("Diegod foi pego pela polícia e perdeu a 50tinha!"),nl, 
+write("Você perdeu a pontuação x2!"), nl, nl, write("Jogar dado (ganha pontos)"), nl, nl.
