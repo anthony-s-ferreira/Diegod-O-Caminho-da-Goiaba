983c77506e83439b58308843e3a1667b10bff1b5
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2022-08-19 12:16:14 -0300

    bug fix duplicated

--- Prolog/Assets.pl
+++ Prolog/Assets.pl
@@ -16,14 +16,13 @@ pocaMortal(X):-
     X = (Mod == 0).
 
 
-event(0,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao):- 
+event(0,CondicaoVida,CheckBonus,Multiplicador,false, Pontuacao, Pontuacao):- 
     HaPontos = (Pontuacao >= 10),
     (
     not(HaPontos) -> boxNoPoints,
                      foundBoxNoPoints,
                      CondicaoVida = true, 
-                     CheckBonus = true, 
-                     QuaseMorreu = true;
+                     CheckBonus = true;
 
     Multiplicador == 1 -> box, 
                            write("Ao abrir esta caixa você perderá a goiaba dourada"), 
@@ -31,8 +30,7 @@ event(0,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao):-
                            nl,
                            foundBox, 
                            CondicaoVida = true, 
-                           CheckBonus = true, 
-                           QuaseMorreu = false;
+                           CheckBonus = true;
 
     Multiplicador == 2 -> box, 
                           write("Ao abrir esta caixa você perderá a cinquentinha"), 
@@ -40,8 +38,7 @@ event(0,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao):-
                           nl,
                           foundBox, 
                           CondicaoVida = true, 
-                          CheckBonus = true, 
-                          QuaseMorreu = false;
+                          CheckBonus = true;
 
     Multiplicador == 3 -> box, 
                           write("Ao abrir esta caixa você perderá a calopsita gigante"), 
@@ -49,121 +46,131 @@ event(0,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao):-
                           nl,
                           foundBox, 
                           CondicaoVida = true, 
-                          CheckBonus = true, 
-                          QuaseMorreu = false;
+                          CheckBonus = true;
 
     Multiplicador == 0 -> box, 
                           foundBox, 
                           CondicaoVida = true, 
-                          CheckBonus = true, 
-                          QuaseMorreu = false).
+                          CheckBonus = true).
 
 
-event(1,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, _):- 
+event(1,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao, Pontuacao):- 
     pocaMortal(X),
     (Multiplicador == 1 -> pocaFunda, 
                            pocaMtFunda, 
                            CondicaoVida = true, 
                            CheckBonus = false, 
-                           QuaseMorreu = true;
+                           QuaseMorreu = true, !;
     
     Multiplicador == 2 -> noEvent, 
                           cinquentinha, 
                           CondicaoVida = true, 
                           CheckBonus = false,
-                          QuaseMorreu = false;
+                          QuaseMorreu = true, !;
 
     Multiplicador == 3 -> noEvent, 
                           calopisitaGigante, 
                           CondicaoVida = true, 
                           CheckBonus = false,
-                          QuaseMorreu = false;
+                          QuaseMorreu = true, !;
 
     X -> derrotaPoca, 
          pocaDefeat, 
          CondicaoVida = false, 
          CheckBonus = false,
-         QuaseMorreu = false;
+         QuaseMorreu = true;
 
     not(X) -> pisaPoca, 
               pocaDoCao, 
               CondicaoVida = true, 
               CheckBonus = false,
-              QuaseMorreu = false).
+              QuaseMorreu = true).
 
-event(2,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao):- 
+event(2,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, Pontuacao, NovaPontuacao):- 
     TemPontos = (Pontuacao >= 10),
 
     (Multiplicador == 1 -> baculejoGoiabaDourada,
                            baculejoGoiaba,
-                           Multiplicador = 0;
+                           QuaseMorreu = true,
+                           NovaPontuacao is Pontuacao;
 
      Multiplicador == 2 -> baculejoMotoquinha,
                            baculejoMoto,
-                           Multiplicador = 0;
+                           QuaseMorreu = true,
+                           NovaPontuacao is Pontuacao;
 
      not(TemPontos), not(Multiplicador == 2), not(Multiplicador == 1) -> derrotaBaculejo,
                                                                          baculejoDefeat,
                                                                          CondicaoVida = false,
                                                                          CheckBonus = false,
                                                                          QuaseMorreu = true,
-                                                                         Pontuacao = Pontuacao;
+                                                                         NovaPontuacao = Pontuacao;
 
-     TemPontos, not(Multiplicador == 2), not(Multiplicador == 1) -> baculejo, 
+     (TemPontos, not(Multiplicador == 2), not(Multiplicador == 1)) -> baculejo, 
                                                                     baculejoSimples,
-                                                                    write("PAssando aquiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"),
-                                                                    removePontos(Pontuacao, 10, PontuacaoPlus),
-                                                                    Pontuacao = PontuacaoPlus
+                                                                    removePontuacao(Pontuacao, PontuacaoPlus),
+                                                                    show(PontuacaoPlus),
+                                                                    CondicaoVida = true,
+                                                                    QuaseMorreu = false,
+                                                                    NovaPontuacao is PontuacaoPlus
     ).
     
 
-event(3, CondicaoVida, CheckBonus, Multiplicador,QuaseMorreu, _):-
+event(3, CondicaoVida, CheckBonus, Multiplicador, false, Pontuacao, Pontuacao):- %nada aconteceu
     noEvent,
     (Multiplicador == 0 -> none,
                            CondicaoVida = true, 
-                           CheckBonus = false,
-                           QuaseMorreu = true;
+                           CheckBonus = false;
 
      Multiplicador == 1 -> noneGoiaba,
                            CondicaoVida = true, 
-                           CheckBonus = false,
-                           QuaseMorreu = true;
+                           CheckBonus = false;
 
      Multiplicador == 2 -> noneCinquentinha,
                            CondicaoVida = true, 
-                           CheckBonus = false,
-                           QuaseMorreu = true;
+                           CheckBonus = false;
 
      Multiplicador == 3 -> noneCalopsita,
                            CondicaoVida = true, 
-                           CheckBonus = false,
-                           QuaseMorreu = true
+                           CheckBonus = false
     ).
 
-event(4,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu, _):- 
-    (Multiplicador == 1 -> ataqueAbeia,
+event(4,CondicaoVida,CheckBonus,1,QuaseMorreu, Pontuacao, Pontuacao):- 
+    ataqueAbeia,
                            ataqueDasAbeia,
                            CondicaoVida = true,
                            CheckBonus = false,
-                           QuaseMorreu = true;
-
-     not(Multiplicador == 1) -> derrotaAbeia, 
+                           QuaseMorreu = true,
+                           write("Retornei a goiaba").
+event(4,CondicaoVida,CheckBonus,_,QuaseMorreu, _, _):- 
+                                derrotaAbeia, 
                                 abeiaDefeat, 
                                 CondicaoVida = false, 
-                                CheckBonus = false, 
-                                QuaseMorreu = true
-    ).
+                                CheckBonus = false,
+                                QuaseMorreu = true,
+                                write("Retornei a morte")
+    .
     
 
-event("Goiaba"):- 
+eventPower("Goiaba"):- 
     goiaba, 
     goibaDourada.
 
-event("Calopsita"):- 
+eventPower("Calopsita"):- 
     calopsita, 
     calopisitaGigante.
 
-event("Motoquinha"):- 
+eventPower("Motoquinha"):- 
     moto, 
     cinquentinha.
 
+
+removePontuacao(Pontuacao,Total):- 
+    Total is Pontuacao - 10.
+    
+
+show(Total):-
+    write("Pontuação "),
+    write(Total),
+    nl, 
+    nl.
--- Prolog/Play.pl
+++ Prolog/Play.pl
@@ -4,7 +4,7 @@
 :- include('Assets.pl').
 :- include('PowerUps.pl').
 
-play(1, Pontuacao, Rodadas, _, Bonus, Multiplicador):-
+play(1, Pontuacao, Rodadas, false, Bonus, Multiplicador):-
     random(1,6,Random),
     write("Você tirou "), 
     write(Random), 
@@ -14,49 +14,65 @@ play(1, Pontuacao, Rodadas, _, Bonus, Multiplicador):-
     %Se tiver pontos a serem multiplicados
     (Bonus > 0 -> Pontos is Multiplicador * Random, 
                   BonusPlus is Bonus -1, 
-                  MultiplicadorPlus = Multiplicador;
+                  MultiplicadorPlus = Multiplicador,
+                  write("------------------------------BOnus >1!!!!!!1------------------------------");
 
-    Bonus == 0, Multiplicador > 1 -> Pontos = Random, 
+    Bonus == 0, Multiplicador >= 1 -> Pontos = Random, 
                          BonusPlus = 0, 
-                         MultiplicadorPlus = 0;
+                         MultiplicadorPlus = 0, write("------------------------------BOnus == 0 multi > 1------------------------------");  
 
-    Bonus == 0, not(Multiplicador > 1) -> Pontos = Random, 
+    Bonus == 0, Multiplicador == 0 -> Pontos = Random, 
     BonusPlus = 0, 
-    MultiplicadorPlus = Multiplicador),
+    MultiplicadorPlus = Multiplicador, write("------------------------------BOnus == 0------------------------------")),
 
     addPontos(Pontuacao,Pontos,PontuacaoAtual), 
 
-    position(Localizacao), 
+    position(EventoAleatorio), 
 
-    event(Localizacao,CondicaoVida,CheckBonus,MultiplicadorPlus,QuaseMorreu, PontuacaoAtual),
-
-    %QuaseMorreu é verdade se ele quase morrer ou perder o bônus
-    (QuaseMorreu -> MultiplicadorPlus = 0, BonusPlus = 0;
-
-    not(QuaseMorreu) -> MultiplicadorPlus = Multiplicador, BonusPlus = Bonus),
-                        RodadasPlus is Rodadas + 1,
-
+    event(EventoAleatorio, CondicaoVida, CheckBonus, MultiplicadorPlus, QuaseMorreu, PontuacaoAtual, NovaPontuacao),
+    write("Saiu do evento"),
+    RodadasPlus is (Rodadas + 1),
+    write("Somou rodada"),
+    (QuaseMorreu -> MultiplicadorPlus = 0; 
+    not(QuaseMorreu) -> MultiplicadorPlus is Multiplicador, write("FALSETED")),
+    write(" checou quase morte"),
     %CondicaoVida for false é pq ele morreu
-    (CondicaoVida -> read(Input),
+    (CondicaoVida -> read(Input), write("------------------------------Leu input------------------------------"),
                      nl,
-                     play(Input, PontuacaoAtual, RodadasPlus, CheckBonus, BonusPlus, MultiplicadorPlus);
+                     
+                     play(Input, NovaPontuacao, RodadasPlus, CheckBonus, BonusPlus, MultiplicadorPlus);
     
-    not(CondicaoVida), RodadasPlus == 1 -> write("Você morreu na primeira rodada, que azar!");
+    not(CondicaoVida), RodadasPlus == 1 -> write("Você perdeu na primeira rodada, que azar!");
 
-    not(CondicaoVida) -> write("Você morreu em "), 
+    not(CondicaoVida) -> write("Você perdeu em "), 
                          write(RodadasPlus), 
                          write(" rodadas!")).
 
 play(2, Pontuacao, Rodadas, true, Bonus, Multiplicador):-
-    (Pontuacao >= 10 -> power_up(BonusPlus,MultiplicadorPowerUp,PowerUpName), 
-                      removePontos(Pontuacao,10,PontosRemovidos),
-                      event(PowerUpName), 
+    (Pontuacao >= 10 ->
+                      write("------------------------------Maior que 10------------------------------"),
+                      power_up(BonusPlus,MultiplicadorPowerUp,PowerUpName), 
+                      removePontos(Pontuacao, PontosRemovidos),
+                      eventPower(PowerUpName), 
                       read(Input), 
                       nl, 
-                      play(Input, PontosRemovidos, Rodadas, false, BonusPlus, MultiplicadorPowerUp);
+                      play(Input, PontosRemovidos, Rodadas, false, BonusPlus, MultiplicadorPowerUp),  
     (Pontuacao < 10) -> nl, 
                           write("Você não tem pontos suficientes"), 
                           nl, 
                           read(Input), 
                           play(Input, Pontuacao, Rodadas, false, Bonus, Multiplicador)).
 
+addPontos(Pontos,Variavel,Total):- 
+    Total is Variavel + Pontos, 
+    write("Pontuação "),
+    write(Total),nl, 
+        nl.
+
+
+removePontos(Pontos,Total):- 
+    Total is Pontos - 10, 
+    write("Pontuação "),
+    write(Total),
+    nl, 
+    nl.
