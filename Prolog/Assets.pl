3d90836ba1ab59f43d80be76eb162aaf050bf1e4
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2022-08-18 21:06:46 -0300

    Refatoramento pt.2

--- Prolog/Assets.pl
+++ Prolog/Assets.pl
@@ -9,96 +9,98 @@
 
 %Para a poça matar ele afogado será gerado um número aleatório de 1 até 100
 %Se esse número for divisível por 3 ele pode morrer afogado
-pocaMortal(X):- random(1,100,N), Y is N mod 3, X = (Y == 0).
+pocaMortal(X):- 
+    random(1,100,Random), 
+    Mod is Random mod 3, 
+    X = (Mod == 0).
 
 
-event(0,R,B,M,D):- 
-    (M == 1 -> box, 
-            write("Ao abrir esta caixa você perderá a goiaba dourada"), 
-            nl, 
-            nl,
-            foundBox, R = true, 
-            B = true, 
-            D = false;
+event(0,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu):- 
+    (Multiplicador == 1 -> box, 
+                           write("Ao abrir esta caixa você perderá a goiaba dourada"), 
+                           nl, 
+                           nl,
+                           foundBox, CondicaoVida = true, 
+                           CheckBonus = true, 
+                           QuaseMorreu = false;
 
-    M == 2 -> box, 
-            write("Ao abrir esta caixa você perderá a cinquentinha"), 
-            nl, 
-            nl,
-            foundBox, 
-            R = true, 
-            B = true, 
-            D = false;
+    Multiplicador == 2 -> box, 
+                          write("Ao abrir esta caixa você perderá a cinquentinha"), 
+                          nl, 
+                          nl,
+                          foundBox, 
+                          CondicaoVida = true, 
+                          CheckBonus = true, 
+                          QuaseMorreu = false;
 
-    M == 3 -> box, 
-            write("Ao abrir esta caixa você perderá a calopsita gigante"), 
-            nl, 
-            nl,
-            foundBox, 
-            R = true, 
-            B = true, 
-            D = false;
+    Multiplicador == 3 -> box, 
+                          write("Ao abrir esta caixa você perderá a calopsita gigante"), 
+                          nl, 
+                          nl,
+                          foundBox, 
+                          CondicaoVida = true, 
+                          CheckBonus = true, 
+                          QuaseMorreu = false;
 
-    M == 0 -> box, 
-            foundBox, 
-            R = true, 
-            B = true, 
-            D = false).
+    Multiplicador == 0 -> box, 
+                          foundBox, 
+                          CondicaoVida = true, 
+                          CheckBonus = true, 
+                          QuaseMorreu = false).
 
 
-event(1,R,B,M,D):- 
+event(1,CondicaoVida,CheckBonus,Multiplicador,QuaseMorreu):- 
     pocaMortal(X),
-
-    (M == 1 -> pocaFunda, 
-            pocaMtFunda, 
-            R = true, 
-            B = false, 
-            D = true;
+    (Multiplicador == 1 -> pocaFunda, 
+                           pocaMtFunda, 
+                           CondicaoVida = true, 
+                           CheckBonus = false, 
+                           QuaseMorreu = true;
     
-    M == 2 -> noEvent, 
-            cinquentinha, 
-            R = true, 
-            B = false,
-            D = false;
+    Multiplicador == 2 -> noEvent, 
+                          cinquentinha, 
+                          CondicaoVida = true, 
+                          CheckBonus = false,
+                          QuaseMorreu = false;
 
-    M == 3 -> noEvent, 
-            calopisitaGigante, 
-            R = true, 
-            B = false,
-            D = false;
+    Multiplicador == 3 -> noEvent, 
+                          calopisitaGigante, 
+                          CondicaoVida = true, 
+                          CheckBonus = false,
+                          QuaseMorreu = false;
 
     X -> derrotaPoca, 
-        pocaDefeat, 
-        R = false, 
-        B = false,
-        D = false;
+         pocaDefeat, 
+         CondicaoVida = false, 
+         CheckBonus = false,
+         QuaseMorreu = false;
 
     not(X) -> pisaPoca, 
-            pocaDoCao, 
-            R = true, 
-            B = false,
-            D = false).
+              pocaDoCao, 
+              CondicaoVida = true, 
+              CheckBonus = false,
+              QuaseMorreu = false).
 
-event(2,R,B,_,D):- 
+event(2,CondicaoVida,CheckBonus,_,QuaseMorreu):- 
     baculejo, 
     baculejoSimples, 
-    R = true, 
-    B = false,
-    D = false.
+    CondicaoVida = true, 
+    CheckBonus = false,
+    QuaseMorreu = false.
 
-event(3,R,B,_,D):- 
+event(3,CondicaoVida,CheckBonus,_,QuaseMorreu):- 
     noEvent, 
     none, 
-    R = true, 
-    B = false,
-    D = false.
+    CondicaoVida = true, 
+    CheckBonus = false,
+    QuaseMorreu = false.
 
-event(4,R,B,_,D):- 
+event(4,CondicaoVida,CheckBonus,_,QuaseMorreu):- 
     derrotaAbeia, 
     abeiaDefeat, 
-    R = false, 
-    B = false, 
-    D = true.
+    CondicaoVida = false, 
+    CheckBonus = false, 
+    QuaseMorreu = true.
 
 event("G"):- 
     goiaba, 
@@ -108,6 +110,6 @@ event("C"):-
     calopsita, 
     calopisitaGigante.
 
-event("M"):- 
+event("Multiplicador"):- 
     moto, 
     cinquentinha.
--- Prolog/Play.pl
+++ Prolog/Play.pl
@@ -13,7 +13,6 @@ play("J", Pontuacao, Rodadas, _, Bonus, Multiplicador):-
     dado(Random),
 
     %Se tiver pontos a serem multiplicados
-
     (Bonus > 0 -> Pontos is Multiplicador * Random, 
                   BonusPlus is Bonus -1, 
                   MultiplicadorPlus = Multiplicador;
@@ -27,25 +26,40 @@ play("J", Pontuacao, Rodadas, _, Bonus, Multiplicador):-
     Bonus == 0 -> Pontos = Random, 
     BonusPlus = 0, 
     MultiplicadorPlus = Multiplicador),
+
     addPontos(Pontuacao,Pontos,PontuacaoAtual), 
+
     position(PontuacaoAtual, Localizacao), 
+
     event(Localizacao,CondicaoVida,CheckBonus,MultiplicadorPlus,QuaseMorreu),
+
     %QuaseMorreu é verdade se ele quase morrer ou perder o bônus
     (QuaseMorreu -> MultiplicadorPlus = 0, BonusPlus = 0;
+
     not(QuaseMorreu) -> MultiplicadorPlus = Multiplicador, BonusPlus = Bonus),
     RodadasPlus is Rodadas + 1,
+
     %CondicaoVida for false é pq ele morreu
-    (CondicaoVida -> read(Input),nl,play(Input, PontuacaoAtual, RodadasPlus, CheckBonus, BonusPlus, MultiplicadorPlus);
-    not(CondicaoVida) -> write("Você morreu em "), write(RodadasPlus), write(" rodadas!")).
+    (CondicaoVida -> read(Input),
+                     nl,
+                     play(Input, PontuacaoAtual, RodadasPlus, CheckBonus, BonusPlus, MultiplicadorPlus);
+    
+    not(CondicaoVida), RodadasPlus == 1 -> write("Você morreu em uma rodada!");
+
+    not(CondicaoVida) -> write("Você morreu em "), 
+                         write(RodadasPlus), 
+                         write(" rodadas!")).
 
 play("ABRIR", Pontuacao, Rodadas, true, Bonus, Multiplicador):-
 (Pontuacao > 10 -> power_up(Pontuacao,Random,MultiplicadorPowerUp,PontuacaoAtual), 
-removePontos(Pontuacao,10,PontosRemovidos),event(PontuacaoAtual), 
-read(Input), 
-nl, 
-play(Input, PontosRemovidos, Rodadas, false, Random, MultiplicadorPowerUp);
-not(Pontuacao > 10) -> nl, write("Você não tem pontos suficientes"), 
-                       nl, 
-                       read(Input), 
-                       play(Input, Pontuacao, Rodadas, false, Bonus, Multiplicador)).
+                   removePontos(Pontuacao,10,PontosRemovidos),
+                   event(PontuacaoAtual), 
+                   read(Input), 
+                   nl, 
+                   play(Input, PontosRemovidos, Rodadas, false, Random, MultiplicadorPowerUp);
+                   not(Pontuacao > 10) -> nl, 
+                                          write("Você não tem pontos suficientes"), 
+                   nl, 
+                   read(Input), 
+                   play(Input, Pontuacao, Rodadas, false, Bonus, Multiplicador)).
 
